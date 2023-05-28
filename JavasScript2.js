8b9aeafc8c5c3eb30f9f6d731c09f8d87c265bdf
UMGjtml99edusgt <jtemalu@miumg.edu.gt>
2023-05-27 22:44:15 -0600

    Delete Commit

--- JavasScript2.js
+++
@@ -1,7 +0,0 @@
-// Scroll header marquee logo navbar heading Section Starts
-
-window.addEventListener("scroll", function(){
-    var header = document.querySelector("header");
-    header.classList.toggle("abajo",window.scrollY>0);
-});
-
